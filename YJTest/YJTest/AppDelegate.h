951128dac778c30704caef9d5d429d44e59d3a4b
早起的鸟儿 <601499739@qq.com>
2017-08-17 23:27:49 +0800

    add project

---
+++ YJTest/YJTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJTest
+//
+//  Created by fighting on 17/8/17.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJTest/YJTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJTest
+//
+//  Created by fighting on 17/8/17.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
